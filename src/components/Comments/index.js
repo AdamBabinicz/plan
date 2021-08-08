ea579ab8a09709d73ff5dace02441ed1f539ff0c
babinicz <babinicz@onet.pl>
2021-08-08 16:49:24 +0200

    zm

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -105,7 +105,7 @@ const Comment = () => {
           >
             <Input type="hidden" name="form-name" value="contact-form" />
             <Input type="text" name="name" placeholder="Imię lub Nick" />
-            <Input id="email" type="email" name="email" placeholder="Email" />
+            <Input type="text" name="email" placeholder="Email" />
             <TextArea
               type="textarea"
               name="message"
